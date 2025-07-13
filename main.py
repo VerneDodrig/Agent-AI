92e458698f7960a902872398e36f6f35e17d9d9e
VerneDodrig <damion-arends@hotmail.com>
2025-07-13 02:18:37 -0700

    added --verbose checks

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
