d2eae4b07f3815ec8da6465a334c0f13218ecdde
VerneDodrig <damion-arends@hotmail.com>
2025-07-13 22:08:08 -0700

    Added writing function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -27,12 +27,37 @@ def get_file_content(working_directory, file_path=None):
     if not full_path.startswith(os.path.abspath(os.path.join(".", working_directory))):
         return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory | Directory Path: {full_path}, Prefix_Path: {os.path.abspath(os.path.join(".", working_directory))}'
     elif not os.path.isfile(full_path):
-        return f'Error: "{file_path}" is not a directory'
+        return f'Error: "{file_path}" is not a file'
     
-    with open(full_path, "r") as file_contents:
-        file_content_string = file_contents.read(MAX_CHARACTERS)
+    try:
+        with open(full_path, "r") as file_contents:
+            file_content_string = file_contents.read(MAX_CHARACTERS)
 
-        if len(file_content_string) == MAX_CHARACTERS:
-            return f'{file_content_string}[...File "{file_path}" truncated at {MAX_CHARACTERS} characters]'
+            if len(file_content_string) == MAX_CHARACTERS:
+                return f'{file_content_string}[...File "{file_path}" truncated at {MAX_CHARACTERS} characters]'
 
-        return f'{file_content_string}'
+            return f'{file_content_string}'
+    except:
+        return f'Error: Unable to open file {file_path} at {full_path}'
+
+
+def write_file(working_directory, file_path, content):
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not full_path.startswith(os.path.abspath(os.path.join(".", working_directory))):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory | Directory Path: {full_path}, Prefix_Path: {os.path.abspath(os.path.join(".", working_directory))}'
+
+    directory_path = os.path.dirname(full_path)
+
+    try:
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+    except:
+        return f'Error: Unable to create directory {directory_path}'
+    
+    try:
+        with open(full_path, "w") as file_to_write:
+            file_to_write.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: Unable to write to {content} to {file_path}.'
--- tests.py
+++ tests.py
@@ -1,9 +1,6 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
-try:
-    print(f'"Result for lorem.txt directory:\n"{get_file_content("calculator", "lorem.txt")}"')
-    print(f'"Result for main.py directory:\n"{get_file_content("calculator", "main.py")}"')
-    print(f'"Result for pkg/calculator.py directory:\n"{get_file_content("calculator", "pkg/calculator.py")}"')
-    print(f'"Result for /bin/cat directory:\n"{get_file_content("calculator", "/bin/cat")}"')
-except Exception as e:
-    print(f'Error: {e}')
+
+print(f'"Result for lorem.txt directory:\n"{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}"')
+print(f'"Result for pkg/morelorem.txt directory:\n"{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}"')
+print(f'"Result for /bin/cat directory:\n"{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}"')
